c2d927121eb46527a470307df01141acb6b17880
Harsh2313 <kaushikh464@gmail.com>
2022-08-04 15:59:09 +0530

    completed

--- src/route/route.js
+++ src/route/route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require("../controller/userController")
 const productController = require("../controller/productController")
 const CartController = require("../controller/cartController")
+const orderController = require("../controller/orderController")
 const middleware = require("../middleware/auth.js")
 
 
@@ -25,6 +26,11 @@ router.post("/users/:userId/cart",middleware.auth,CartController. createCart );
 router.put("/users/:userId/cart",middleware.auth, CartController.updateCart);
 router.get("/users/:userId/cart",middleware.auth, CartController.getCart);
 router.delete("/users/:userId/cart",middleware.auth, CartController.deleteCart);
+
+//---------------------------------orderModel----------------------------------------------------//
+router.post("/users/:userId/orders",middleware.auth,orderController.createOrder);
+
+router.put("/users/:userId/orders",middleware.auth,orderController.updateOrder);
 
 
 
